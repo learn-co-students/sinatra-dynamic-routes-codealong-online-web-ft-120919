3baae9a67a4ea831ad1f7b13379775d8a535a302
mrarthurwhite <mister.arthur.white@gmail.com>
2020-02-04 13:30:27 -0500

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,18 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
-
   # Code your final two routes here:
+
+  # This is a dynamic route.
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get "/multiply/:x/:y" do
+    @result = params[:x].to_i * params[:y].to_i
+    "#{@result}"
+  end
+
 
 end
